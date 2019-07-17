51cd05a009d445b2e442c06daff139d6e5755ddb
longjiang <zlj940323@163.com>
2019-07-17 18:48:16 +0800

    asdf

--- vue-project/src/views/editPages/edit.vue
+++ vue-project/src/views/editPages/edit.vue
@@ -1,77 +1,82 @@
 <template>
-    <el-form ref="form" :model="form" label-width="200px">
-        <p>{{form}}</p>
-        <el-form-item label="Page:" prop="Page">
-            <el-col :span="4">
-                <span>{{form.page}}</span>
-                <span>{{type}}</span>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Key:" prop="name">
-            <el-col :span="8">
-                <el-input v-model="form.key" placeholder="Element Name"></el-input>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Type:" prop="type">
-            <el-col :span="8">
-                <select v-model="form.type" name="" id="">
-                    <option v-for="item in typeList">{{item.code}}</option>
-                </select>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Ext Data:" prop="Ext">
-            <el-col :span="8">
-                <el-input v-model="form.data" placeholder="Ext Data"></el-input>
-            </el-col>
-        </el-form-item>
-         <el-form-item label="Required:" prop="Required">
-            <el-radio-group v-model="form.isRequired">
-                <el-radio label="1">Yes</el-radio>
-                <el-radio label="0">No</el-radio>
-            </el-radio-group>
-        </el-form-item>
-        <el-form-item label="Disable:" prop="Disable">
-            <el-radio-group v-model="form.isDisable">
-                <el-radio label="1">Yes</el-radio>
-                <el-radio label="0">No</el-radio>
-            </el-radio-group>
-        </el-form-item>
-        <el-form-item label="en_US:" prop="en_US">
-            <el-col :span="8">
-                <el-input v-model="form.enUs" placeholder="English Name"></el-input>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="zh_cn:" prop="zh_CN">
-            <el-col :span="8">
-                <el-input v-model="form.zhCn" placeholder="Chinese Name"></el-input>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Place Holder:" prop="PlaceHolder">
-            <el-col :span="8">
-                <el-input v-model="form.placeHolder" placeholder="Element placeholder"></el-input>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Default Value:" prop="DefaultValue">
-            <el-col :span="8">
-                <el-input v-model="form.defaultValue" placeholder="Element DefaultValue"></el-input>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Constraint Condition:" prop="Constraint">
-            <el-col :span="8">
-                <el-input v-model="form.constCondition" placeholder="Max length"></el-input>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Order:" prop="Order">
-            <el-col :span="8">
-                <el-input v-model="form.idx" placeholder="Element order"></el-input>
-            </el-col>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="submit">Save</el-button>
-            <el-button @click="onCancel">Cancel</el-button>
-        </el-form-item>
-    </el-form>
+<el-col :span="12" :offset="6">
+    <el-card shadow="hover" style="margin-bottom:100px;font-family:Poppins;">
+        <el-form ref="form" :model="form" label-width="200px">
+            <p>{{form}}</p>
+            <el-form-item label="Page:" prop="Page">
+                <el-col :span="4">
+                    <span>{{form.page}}</span>
+                    <span>{{type}}</span>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Key:" prop="name">
+                <el-col :span="16">
+                    <el-input v-model="form.key" placeholder="Element Name"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Type:" prop="type">
+                <el-col :span="16">
+                    <select v-model="form.type" name="" id="">
+                        <option v-for="item in typeList" :key="item.code" selected="item.code == form.type">{{item.value}}</option>
+                    </select>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Ext Data:" prop="Ext">
+                <el-col :span="16">
+                    <el-input v-model="form.data" placeholder="Ext Data"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Required:" prop="Required">
+                <el-radio-group v-model="form.isRequired">
+                    <el-radio label="1">Yes</el-radio>
+                    <el-radio label="0">No</el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="Disable:" prop="Disable">
+                <el-radio-group v-model="form.isDisable">
+                    <el-radio label="1">Yes</el-radio>
+                    <el-radio label="0">No</el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="en_US:" prop="en_US">
+                <el-col :span="16">
+                    <el-input v-model="form.enUs" placeholder="English Name"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="zh_cn:" prop="zh_CN">
+                <el-col :span="16">
+                    <el-input v-model="form.zhCn" placeholder="Chinese Name"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Place Holder:" prop="PlaceHolder">
+                <el-col :span="16">
+                    <el-input v-model="form.placeHolder" placeholder="Element placeholder"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Default Value:" prop="DefaultValue">
+                <el-col :span="16">
+                    <el-input v-model="form.defaultValue" placeholder="Element DefaultValue"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Constraint Condition:" prop="Constraint">
+                <el-col :span="16">
+                    <el-input v-model="form.constCondition" placeholder="Max length"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="Order:" prop="Order">
+                <el-col :span="16">
+                    <el-input v-model="form.idx" placeholder="Element order"></el-input>
+                </el-col>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submit">Save</el-button>
+                <el-button @click="onCancel">Cancel</el-button>
+            </el-form-item>
+        </el-form>
+    </el-card>
+</el-col>
 </template>
+
 <script>
 import {
     saveProjectInfo,
@@ -84,8 +89,7 @@ export default {
             tableHead: ['Key', 'Type', 'Ext.Data', 'Required', 'Disable', 'en_US', 'zh_CN', 'Place Holder', 'Default Value', 'Constraint', 'Order'],
             radio1: '1',
             radio2: '1',
-            form: {
-            },
+            form: {},
             typeList: []
         }
     },
@@ -123,30 +127,30 @@ export default {
         onCancel() {
             this.$router.go(-1);
         },
-        submit(){
+        submit() {
             var formData = this.form;
-            this.Fetch('/ui/updateUiElement?token=111&page=product', {
-                formData
-            }).then(res => {
-                console.log(res);
-            });
+            // this.Fetch('/ui/updateUiElement?token=111&page=product', {
+            //     formData
+            // }).then(res => {
+            //     console.log(res);
+            // });
             // axios
-            // this.$axios.post('/ui/updateUiElement?token=111&page=product',formData).then(res => {
-            //         // success callback
-            //         console.log(formData);
-            //         console.log(res);
+            this.$axios.post('/ui/updateUiElement?token=111&page=product', formData).then(res => {
+                // success callback
+                console.log(formData);
+                console.log(res);
 
-            //         var ec = res.ec;
-            //         debugger;
-            //         if(ec != '0') {
-            //             alert(res.ec + ", " + res.em);
-            //         } else {
-            //             this.$message.success("保存成功！");
-            //             this.$router.go(-1);
-            //         }
-            //     }).catch(err => {
-            //        alert(err);
-            //     });
+                var ec = res.data.ec;
+                debugger;
+                if (ec != '0') {
+                    alert(res.ec + ", " + res.em);
+                } else {
+                    this.$message.success("保存成功！");
+                    this.$router.go(-1);
+                }
+            }).catch(err => {
+                alert(err);
+            });
         }
     }
 }
--- vue-project/src/views/editPages/index.vue
+++ vue-project/src/views/editPages/index.vue
@@ -2,12 +2,12 @@
 <div class="main" style="width:75%;margin-left:12.5%">
     <div class="top" style="margin:25px 0">
         <span style="padding-left:80px;font-family:Poppins;">Choose Page:</span>
-        <select   v-model="page"  id="" style="margin-left:20px">
-          <option v-for="item in pageList">{{item.value}}</option>
+        <select v-model="page" id="" style="margin-left:20px">
+            <option v-for="item in pageList">{{item.value}}</option>
         </select>
-            <el-button @click="addRouter" style="float:right;margin-right:10%;color:white;background-color:#7a85a1;line-heigt:32px;width:100px;">
-                + Add
-            </el-button>
+        <el-button @click="addRouter" style="float:right;margin-right:10%;color:white;background-color:#7a85a1;line-heigt:32px;width:100px;">
+            + Add
+        </el-button>
     </div>
     <table class="common-table" style="width:100%">
         <thead class="tableHead">
@@ -18,13 +18,13 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="tableBody" v-for="items in tableData" >
-                <td v-for="(item,index) in items" v-if="['page','label','dispStatus','version','validation','display'].indexOf(index) === -1">
+            <tr class="tableBody" v-for="items in tableData">
+                <td v-for="(item,index) in items" v-if="['dbField','page','label','dispStatus','version','validation','display'].indexOf(index) === -1">
                     <p>{{item}}</p>
                 </td>
-                <td  :order="items.idx">
+                <td :order="items.idx">
                     <el-button @click="returnTrIndex();editRouter()" :order="items.idx" class="editDeleteBtn" icon="el-icon-edit" circle></el-button>
-                    <el-button class="editDeleteBtn" type="danger" icon="el-icon-delete" circle></el-button>
+                    <el-button @click="returnTrIndex();deleteTr()" :order="items.idx" class="editDeleteBtn" type="danger" icon="el-icon-delete" circle></el-button>
                 </td>
             </tr>
         </tbody>
@@ -41,15 +41,15 @@ export default {
             pageList: [],
             tableData: [],
             page: '',
-            trIndex:'123'
+            trIndex: '123'
         }
     },
     created() {
-        this.Fetch('/ui/getUiInfoPage?token=111', {
+        this.Fetch('/setting/getUiInfoPage?token=111&type=Page_Element_Setting', {
             method: 'GET'
-        }).then(res =>{
-          this.pageList = res.cd;
-          this.page = res.cd[0].value;
+        }).then(res => {
+            this.pageList = res.cd;
+            this.page = res.cd[0].value;
         });
         // Fetch("/mock/menu/getPageList", {
         //     method: "GET"
@@ -71,37 +71,60 @@ export default {
         page: function (val) {
             console.log(val);
             //get the page UIUIInfoList
-            this.Fetch("/ui/getUiInfoList?token=111&page=product", {
+            this.Fetch(`/ui/getUiInfoList?token=111&page=${this.page}`, {
                 method: "GET"
             }).then(data => {
                 console.log(data.cd);
                 this.tableData = data.cd;
             });
-        }
+        },
+
     },
     methods: {
-      addRouter(){
-        this.$router.push({ 
-            path: 'add' ,
-            name: 'add',
-            params: {
-                        page: this.page
-                    }
-        })
-      },
-      editRouter(){
-        this.$router.push({ 
-            path: 'edit' ,
-            name: 'edit',
-            params: {
-                        page: this.page,
-                        tr: this.tableData[this.trIndex-1],//这里的数字要根据点击的是第几行
-                    }
-        })
-      },
-      returnTrIndex(){
-          this.trIndex = event.target.parentNode.getAttribute('order');
-      }
+        beforeRemove(file, fileList) {
+            return this.$confirm(`确定移除 ${file.name}？`)
+        },
+        addRouter() {
+            this.$router.push({
+                path: 'add',
+                name: 'add',
+                params: {
+                    page: this.page
+                }
+            })
+        },
+        editRouter() {
+            this.$router.push({
+                path: 'edit',
+                name: 'edit',
+                params: {
+                    page: this.page,
+                    tr: this.tableData[this.trIndex - 1], //这里的数字要根据点击的是第几行
+                }
+            })
+        },
+        returnTrIndex() {
+            return this.trIndex = event.target.parentNode.getAttribute('order');
+        },
+        deleteTr() {
+            // var tr = this.tableData[this.trIndex - 1].id;
+            // console.log(tr);    
+            // console.log(typeof(tr));
+            this.$axios.post(`/ui/removeUiElement?token=111&id=26`).then(res => {//token先是写死的
+                // success callback
+                console.log(res);
+                var ec = res.data.ec;
+                debugger;
+                if (ec != '0') {
+                    alert(res.ec + ", " + res.em);
+                } else {
+                    this.$message.success("Delete Sucess！");
+                    console.log(this.tableData);
+                }
+            }).catch(err => {
+                alert(err);
+            });
+        }
     }
 }
 </script>
@@ -109,7 +132,6 @@ export default {
 <style scoped>
 .main {
     height: 600px;
-    border: 1px solid red;
 }
 
 .chooseTd {
