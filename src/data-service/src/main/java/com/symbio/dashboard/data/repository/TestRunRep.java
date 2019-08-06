cbeedd3ce35bb118dd1cd2cc426ec4e5793b83bd
Danny Chen <xiaotong.chen@symbio.com>
2019-08-06 18:03:58 +0800

    Repository fix

--- src/data-service/src/main/java/com/symbio/dashboard/data/repository/TestRunRep.java
+++ src/data-service/src/main/java/com/symbio/dashboard/data/repository/TestRunRep.java
@@ -2,8 +2,10 @@ package com.symbio.dashboard.data.repository;
 
 import com.symbio.dashboard.model.TestRun;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TestRunRep extends JpaRepository<TestRunRep, Integer> {
+@Repository
+public interface TestRunRep extends JpaRepository<TestRun, Integer> {
 
     TestRun getById(Integer id);
 
