7e87b618f21199399033c67123e2b52e794af78f
longjiang <zlj940323@163.com>
2019-10-10 18:26:18 +0800

    font and runDrop

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -58,9 +58,9 @@ module.exports = {
         hotOnly: false,
         proxy: {
             '/api': {
-                target: "http://127.0.0.1:9090",//serve
+                // target: "http://127.0.0.1:9090",//serve
                 // target: "http://172.27.24.62:9090",//haoge
-                // target: "http://192.168.170.100:9090",//local
+                target: "http://192.168.170.100:9090",//local
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
