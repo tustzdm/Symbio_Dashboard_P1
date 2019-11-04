a66280b868e07a7486b2c16f7d2fa06b7b8406d7
longjiang <zlj940323@163.com>
2019-11-04 18:51:52 +0800

    draw rect

--- vue-project/src/views/results/paint.vue
+++ vue-project/src/views/results/paint.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <canvas id="theCanvas" width=300 height=620 style="border:1px solid gray;display:inline-block">
+    <canvas id="theCanvas" @mousedown="drawLine()" width=300 height=620 style="border:1px solid gray;display:inline-block">
         <textarea name="" id="" cols="30" rows="10"></textarea>
     </canvas>
 
@@ -8,12 +8,19 @@
         <ul>
             <li> <i class="el-icon-edit pen" :class="{penActive:lineWidth==3}" @click="setLineWidth(3)" style="font-size:10px;padding:4px"></i></li>
             <li><i class="el-icon-edit pen" :class="{penActive:lineWidth==8}" @click="setLineWidth(8)" style="font-size:15px;padding:2px"></i></li>
-            <li><i class="el-icon-edit pen" :class="{penActive:lineWidth==13}" @click="setLineWidth(13)" style="font-size:18px;pading:px"></i></li>
+            <li><i class="el-icon-edit pen" :class="{penActive:lineWidth==13}" @click="setLineWidth(13)" style="font-size:18px;"></i></li>
         </ul>
         <ul>
             <li>
                 <el-color-picker v-model="color"></el-color-picker>
             </li>
+            
+            <li style="margin-bottom:6px">
+                <el-button @click="ifRect=true" style="padding:4px">
+                    <div style="width:12px;height:12px;padding:2px;border:1px solid black;background:#99E7F7"></div>
+                </el-button>
+            </li>
+
             <li>
                 <el-button style="padding:3px" @click="setText();"><i class="el-icon-s-comment" style="font-size:20px"></i></el-button>
             </li>
@@ -33,7 +40,8 @@ export default {
             imgEdit: this.url,
             text: 'sSSSSSSSSSS',
             textarea: null,
-            commentClicked: false
+            commentClicked: false,
+            ifRect: false
         }
     },
     props: {
@@ -43,59 +51,11 @@ export default {
 
     },
     mounted() {
-        let theCanvas = document.querySelector('#theCanvas')
-        //add img
-        var imgObj = new Image();
-        imgObj.src = this.imgEdit;
-        //待图片加载完后，将其显示在canvas上
-        imgObj.onload = function () {
-            var ctx = theCanvas.getContext('2d');
-            // ctx.drawImage(this, 0, 0); //this即是imgObj,保持图片的原始大小：470*480
+        let theCanvas = document.querySelector('#theCanvas');
+        this.drawPic();
 
-            theCanvas.width = imgObj.width;
-            theCanvas.height = imgObj.height;
-            ctx.drawImage(this, 0, 0); //改变图片的大小到1024*768
-        }
         let context = theCanvas.getContext('2d')
         context.lineWidth = this.lineWidth;
-
-        if (!theCanvas || !theCanvas.getContext) {
-            return false
-        } else {
-            let context = theCanvas.getContext('2d')
-            let isAllowDrawLine = false
-            theCanvas.onmousedown = (e) => {
-                context.lineWidth = this.lineWidth
-                isAllowDrawLine = true
-                let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
-                let {
-                    x,
-                    y
-                } = ele;
-                console.log(ele)
-                context.moveTo(x, y)
-                theCanvas.onmousemove = (e) => {
-                    if (isAllowDrawLine) {
-                        let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
-                        let {
-                            x,
-                            y
-                        } = ele
-                        context.lineTo(x, y)
-                        context.stroke()
-                    }
-                }
-            }
-            theCanvas.onmouseup = (e)=> {
-                isAllowDrawLine = false
-                let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
-                let {
-                    x,
-                    y
-                } = ele;
-                console.log(ele)
-            }
-        }
     },
     destroyed() {},
     computed: {
@@ -115,6 +75,64 @@ export default {
             return {
                 x: x - rect.left * (canvas.width / rect.width),
                 y: y - rect.top * (canvas.height / rect.height)
+            }
+        },
+        drawPic() {
+            //add img
+            var imgObj = new Image();
+            imgObj.src = this.imgEdit;
+            //待图片加载完后，将其显示在canvas上
+            imgObj.onload = function () {
+                var ctx = theCanvas.getContext('2d');
+                // ctx.drawImage(this, 0, 0); //this即是imgObj,保持图片的原始大小：470*480
+
+                theCanvas.width = imgObj.width;
+                theCanvas.height = imgObj.height;
+                ctx.drawImage(this, 0, 0); //改变图片的大小到1024*768
+            }
+        },
+        drawLine() {
+            if (this.ifRect) { //加一个判断，当不画矩形的时候继续画线，画矩形这里就返回不继续执行
+                this.drawRect()
+                return
+            }
+            if (!theCanvas || !theCanvas.getContext) {
+                return false
+            } else {
+                console.log(12312351343312)
+                let context = theCanvas.getContext('2d')
+                let isAllowDrawLine = false
+                theCanvas.onmousedown = (e) => {
+                    context.lineWidth = this.lineWidth
+                    isAllowDrawLine = true
+                    let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
+                    let {
+                        x,
+                        y
+                    } = ele;
+                    console.log(ele)
+                    context.moveTo(x, y)
+                    theCanvas.onmousemove = (e) => {
+                        if (isAllowDrawLine) {
+                            let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
+                            let {
+                                x,
+                                y
+                            } = ele
+                            context.lineTo(x, y)
+                            context.stroke()
+                        }
+                    }
+                }
+                theCanvas.onmouseup = (e) => {
+                    isAllowDrawLine = false
+                    let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
+                    let {
+                        x,
+                        y
+                    } = ele;
+                    console.log(ele)
+                }
             }
         },
         setLineWidth(v) {
@@ -156,7 +174,33 @@ export default {
             }
 
         },
+        drawRect() {
+            let theCanvas = document.querySelector('#theCanvas');
+            let context = theCanvas.getContext('2d');
+            context.beginPath();
+            let x, y, w, h;
 
+            theCanvas.onmousedown = (e) => {
+                context.lineWidth = this.lineWidth
+                let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
+                x = ele.x;
+                y = ele.y;
+                console.log(x)
+                console.log(y)
+            }
+            theCanvas.onmouseup = (e) => {
+                let ele = this.windowToCanvas(theCanvas, e.clientX, e.clientY)
+                w = ele.x - x;
+                h = ele.y - y;
+                console.log(w)
+                console.log(h)
+                context.rect(x, y, w, h);
+                console.log(x, y, w, h)
+                context.stroke();
+            }
+
+            console.log(1111)
+        }
     }
 }
 </script>
